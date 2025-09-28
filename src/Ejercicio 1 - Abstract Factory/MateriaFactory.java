cc9386a924bad7de443ac07fe2d26033512f51b2
Eileen Rodriguez <brillithgualtero@gmail.com>
2025-09-28 13:07:11 -0500

    feat:: crear clase MateriaFactory para crear y guardar materias

---
+++ src/Ejercicio 1 - Abstract Factory/MateriaFactory.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MateriaFactory {
+    private List<Materia> materias;
+
+    public MateriaFactory() {
+        this.materias = new ArrayList<>();
+    }
+
+    public List<Materia> getMaterias() {
+        return materias;
+    }
+
+    public Materia crearMateria(String nombre, int cuposTotales, String horario) {
+        Materia m = new Materia(nombre, cuposTotales, horario);
+        materias.add(m);
+        return m;
+    }
+}
