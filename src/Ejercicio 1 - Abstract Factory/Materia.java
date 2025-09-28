5b159b6c1fd5eb8a45740e98487d939b9f4faa36
Eileen Rodriguez <brillithgualtero@gmail.com>
2025-09-28 13:06:01 -0500

    feat: crear clase Materia

---
+++ src/Ejercicio 1 - Abstract Factory/Materia.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Materia {
+    private String nombre;
+    private int cuposTotales;
+    private int cuposDisponibles;
+    private String horario;
+    private List<Estudiante> inscritos;
+
+    public Materia(String nombre, int cuposTotales, String horario) {
+        this.nombre = nombre;
+        this.cuposTotales = cuposTotales;
+        this.cuposDisponibles = cuposTotales;
+        this.horario = horario;
+        this.inscritos = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCuposTotales() {
+        return cuposTotales;
+    }
+
+    public void setCuposTotales(int cuposTotales) {
+        this.cuposTotales = cuposTotales;
+    }
+
+    public int getCuposDisponibles() {
+        return cuposDisponibles;
+    }
+
+    public void setCuposDisponibles(int cuposDisponibles) {
+        this.cuposDisponibles = cuposDisponibles;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public List<Estudiante> getInscritos() {
+        return inscritos;
+    }
+
+
+    public boolean hayCupos() {
+        return cuposDisponibles > 0;
+    }
+
+    public boolean inscribirEstudiante(Estudiante e) {
+        if (hayCupos() && !inscritos.contains(e)) {
+            inscritos.add(e);
+            cuposDisponibles--;
+            System.out.println(e.getNombre() + " se inscribió en " + nombre);
+            return true;
+        }
+        System.out.println("No hay cupos disponibles en " + nombre + " para " + e.getNombre());
+        return false;
+    }
+    
+
+
+    public void mostrarInscritos() {
+    System.out.println("Materia: " + nombre);
+    if (inscritos.isEmpty()) {
+        System.out.println("  No hay estudiantes inscritos.");
+    } else {
+        System.out.println("  Estudiantes inscritos:");
+        for (Estudiante e : inscritos) {
+            System.out.println("   - " + e.getNombre());
+        }
+    }
+}
+
+
+}
+
